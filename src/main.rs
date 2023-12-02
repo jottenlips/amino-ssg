b92a759033b1e38a1f74021ee68ad1e9e9d8d87e
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2023-12-02 17:15:22 -0600

    remove logs

--- src/main.rs
+++ src/main.rs
@@ -9,32 +9,27 @@ fn main() {
     recursiveReadDir(baseDir);
 }
 
-fn recursiveReadDir(dir: &str) {
+fn recursiveReadDir(dir: &str, baseHtml: &str) {
     let paths = fs::read_dir(dir).unwrap();
-    println!("In dir: {}", dir);
+    let mut html = baseHtml;
     for path in paths {
         let pathname = path.unwrap().path().display().to_string();
         let pathBuf = PathBuf::from(pathname.clone());
-        println!("Name: {}", pathname);
-
         if pathBuf.is_dir() {
-            println!("Recursion: {}", pathname);
             recursiveReadDir(&pathname);
         } else {
             if !pathname.ends_with(".md") {
-                println!("Not a markdown file");
                 continue;
             }
             let input = fs
                 ::read_to_string(&pathname)
                 .expect("Should have been able to read the file");
-
             // Processes Markdown without looking for frontmatter
             let Ok(HTMLOutput { content, toc, .. }) = process_markdown_to_html(&input) else {
                 println!("Error");
                 return;
             };
-            println!("Title: {}", content);
+            println!("page: {}", pathname);
         }
     }
 }
