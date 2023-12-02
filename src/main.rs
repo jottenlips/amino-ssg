96c5e85365524fd25d9857683169d0fb8ea4100f
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2023-12-02 17:52:20 -0600

    embed content in HTML

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,45 @@ use femark::{ HTMLOutput, process_markdown_to_html };
 use std::fs;
 // use std::fs::metadata;
 use std::path::PathBuf;
-const baseDir: &str = "./markdown";
+// const baseDir: &str = "./markdown";
 
 fn main() {
-    recursiveReadDir(baseDir);
+    let markdownDir = std::env::args().nth(1).expect("no markdown directory given");
+    let out = std::env::args().nth(2).expect("no out dir given");
+
+    recursiveReadDir(&markdownDir, "");
 }
 
-fn recursiveReadDir(dir: &str, baseHtml: &str) {
+fn getBaseHtml<'a>(dir: &'a str, baseHtml: &'a str) -> String {
     let paths = fs::read_dir(dir).unwrap();
-    let mut html = baseHtml;
+
     for path in paths {
         let pathname = path.unwrap().path().display().to_string();
         let pathBuf = PathBuf::from(pathname.clone());
         if pathBuf.is_dir() {
-            recursiveReadDir(&pathname);
+            continue;
+        } else {
+            if pathname.ends_with("base.html") {
+                // return html read in
+                let input = fs
+                    ::read_to_string(&pathname)
+                    .expect("Should have been able to read the file");
+                return input;
+            }
+        }
+    }
+    return baseHtml.to_string();
+}
+
+fn recursiveReadDir(dir: &str, baseHtml: &str) {
+    let html = getBaseHtml(dir, baseHtml);
+    println!("html: {}", html);
+    let paths = fs::read_dir(dir).unwrap();
+    for path in paths {
+        let pathname = path.unwrap().path().display().to_string();
+        let pathBuf = PathBuf::from(pathname.clone());
+        if pathBuf.is_dir() {
+            recursiveReadDir(&pathname, &html);
         } else {
             if !pathname.ends_with(".md") {
                 continue;
@@ -29,7 +54,8 @@ fn recursiveReadDir(dir: &str, baseHtml: &str) {
                 println!("Error");
                 return;
             };
-            println!("page: {}", pathname);
+            let page = html.replace("<!-- MARKDOWN -->", &content);
+            println!("page: {}", page);
         }
     }
 }
