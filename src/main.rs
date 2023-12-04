4a3791d83334c353dbc864051bfa3768b20b7ddf
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2023-12-03 21:36:25 -0600

    SSG works like old algo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use femark::{ HTMLOutput, process_markdown_to_html };
 // use std::env;
 use std::fs;
+use crate::fs::File;
+use std::io::Write;
 // use std::fs::metadata;
 use std::path::PathBuf;
 // const baseDir: &str = "./markdown";
@@ -8,8 +10,13 @@ use std::path::PathBuf;
 fn main() {
     let markdownDir = std::env::args().nth(1).expect("no markdown directory given");
     let out = std::env::args().nth(2).expect("no out dir given");
+    // create an out dir if it doesn't exist
+    let outDir = PathBuf::from(&out);
+    if !outDir.exists() {
+        fs::create_dir_all(&outDir).expect("Could not create out dir");
+    }
 
-    recursiveReadDir(&markdownDir, "");
+    recursiveReadDir(&markdownDir, "", &out);
 }
 
 fn getBaseHtml<'a>(dir: &'a str, baseHtml: &'a str) -> String {
@@ -33,16 +40,48 @@ fn getBaseHtml<'a>(dir: &'a str, baseHtml: &'a str) -> String {
     return baseHtml.to_string();
 }
 
-fn recursiveReadDir(dir: &str, baseHtml: &str) {
-    let html = getBaseHtml(dir, baseHtml);
-    println!("html: {}", html);
+fn createDirs(dir: &str, out: &str) {
     let paths = fs::read_dir(dir).unwrap();
+
     for path in paths {
         let pathname = path.unwrap().path().display().to_string();
         let pathBuf = PathBuf::from(pathname.clone());
         if pathBuf.is_dir() {
-            recursiveReadDir(&pathname, &html);
+            // create a dir in the out dir
+            let outDir = [out, "/"].join("");
+            let dirString = pathBuf.file_name().unwrap().to_str().unwrap().to_string();
+            let dirPath = [outDir, dirString].join("");
+            fs::create_dir_all(&dirPath).expect("Could not create out dir");
+            println!("dirPath: {}", dirPath);
+            createDirs(&pathname, &out);
+        }
+    }
+}
+
+fn recursiveReadDir(dir: &str, baseHtml: &str, out: &str) {
+    let html = getBaseHtml(dir, baseHtml);
+    let paths = fs::read_dir(dir).unwrap();
+    createDirs(dir, out);
+
+    for path in paths {
+        let pathname = path.unwrap().path().display().to_string();
+        let pathBuf = PathBuf::from(pathname.clone());
+        if pathBuf.is_dir() {
+            recursiveReadDir(&pathname, &html, &out);
         } else {
+            if pathname.ends_with("styles.css") {
+                // create a styles file in the out dir
+                let outDir = [out, "/"].join("");
+                let stylesString = "styles.css".to_string();
+
+                let stylesPath = [outDir, stylesString].join("");
+                let mut styles_file = File::create(stylesPath.clone()).expect("creation failed");
+                let styles = fs
+                    ::read_to_string(&pathname)
+                    .expect("Should have been able to read the file");
+                styles_file.write(styles.as_bytes()).expect("write failed");
+                continue;
+            }
             if !pathname.ends_with(".md") {
                 continue;
             }
@@ -55,7 +94,26 @@ fn recursiveReadDir(dir: &str, baseHtml: &str) {
                 return;
             };
             let page = html.replace("<!-- MARKDOWN -->", &content);
-            println!("page: {}", page);
+            // println!("page: {}", page);
+            // Create a file
+            let outDir = [out, "/"].join("");
+            let indexHtmlString = "index.html".to_string();
+            let indexMarkdownString = "index.md".to_string();
+            let homePage = [dir, "/", &indexMarkdownString].join("");
+            let indexHtmlPath = match pathname {
+                ref pathname if pathname == &homePage => [outDir, indexHtmlString].join(""),
+                _ => { pathname.replace(".md", "/index.html").replace("markdown/", &outDir) }
+            };
+
+            let prettyPathDirName = indexHtmlPath.replace("index.html", "");
+            // create path if it doesn't exist
+            if !PathBuf::from(&prettyPathDirName).exists() {
+                fs::create_dir_all(&prettyPathDirName).expect("Could not create out dir");
+            }
+            println!("indexHtmlPath: {}", indexHtmlPath);
+            let mut data_file = File::create(indexHtmlPath.clone()).expect("creation failed");
+            // Write contents to the file
+            data_file.write(page.as_bytes()).expect("write failed");
         }
     }
 }
